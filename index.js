b350572b9901abca9b0017768257629a6c28374b
giguchi <hil.giga@gmail.com>
2022-11-06 22:59:22 +0000

    configure GridFsStorage for image uploading. Implement image routing/uploading (post/get) functionality

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cors = require("cors")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const mongoose = require("mongoose")
+const crypto = require("crypto")
+const multer = require("multer")
+const {GridFsStorage} = require("multer-gridfs-storage")
 const dotenv = require("dotenv").config()
 
 const userRoute = require("./routes/users")
@@ -10,6 +13,8 @@ const authRoute = require("./routes/login")
 const postRoute = require("./routes/posts")
 const conversationRoute = require("./routes/conversation")
 const messageRoute = require("./routes/message")
+const imageRoute = require("./routes/image")
+const path = require("path");
 
 const app = express()
 
@@ -22,11 +27,38 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 })
 
 
+
+
+const storage = new GridFsStorage({
+    url: process.env.MONGO_URL,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buff) =>{
+                if(err) {
+                    return reject(err)
+                }
+
+                const fileName = buff.toString("hex") + path.extname(file.originalname)
+                const fileInfo = {
+                    fileName: fileName,
+                    bucketName:"uploads"
+                }
+                resolve(fileInfo)
+            })
+        })
+    }
+ })
+
+const upload = multer({storage})
+
+
+
 // Middleware
 app.use(cors())
 app.use(express.json())
-app.use(helmet())
+app.use(helmet({crossOriginResourcePolicy: false}))
 app.use(morgan("common"))
+
 
 
 app.use("/api/users", userRoute)
@@ -34,6 +66,7 @@ app.use("/api/auth", authRoute)
 app.use("/api/posts", postRoute)
 app.use("/api/conversations", conversationRoute)
 app.use("/api/messages", messageRoute)
+app.use("/api/upload", imageRoute(upload))
 
 
 const PORT = process.env.PORT
