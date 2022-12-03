12350587bf8cb28c842606989e1f3811743ad7eb
giguchi <hil.giga@gmail.com>
2022-12-03 03:34:36 +0000

    tested: works locally - netlify dev

--- netlify/functions/index.js
+++ netlify/functions/index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const morgan = require("morgan")
 const helmet = require("helmet")
 const mongoose = require("mongoose")
-// const crypto = require("crypto")
+const crypto = require("crypto")
 const multer = require("multer")
 const {GridFsStorage} = require("multer-gridfs-storage")
 const dotenv = require("dotenv").config()
@@ -29,27 +29,27 @@ mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopolo
 })
 
 
-// const storage = new GridFsStorage({
-//     url: process.env.MONGO_URL,
-//     file: (req, file) => {
-//         return new Promise((resolve, reject) => {
-//             crypto.randomBytes(16, (err, buff) =>{
-//                 if(err) {
-//                     return reject(err)
-//                 }
-//
-//                 const fileName = buff.toString("hex") + path.extname(file.originalname)
-//                 const fileInfo = {
-//                     fileName: fileName,
-//                     bucketName:"uploads"
-//                 }
-//                 resolve(fileInfo)
-//             })
-//         })
-//     }
-//  })
+const storage = new GridFsStorage({
+    url: process.env.MONGO_URL,
+    file: (req, file) => {
+        return new Promise((resolve, reject) => {
+            crypto.randomBytes(16, (err, buff) =>{
+                if(err) {
+                    return reject(err)
+                }
 
-// const upload = multer({storage})
+                const fileName = buff.toString("hex") + path.extname(file.originalname)
+                const fileInfo = {
+                    fileName: fileName,
+                    bucketName:"uploads"
+                }
+                resolve(fileInfo)
+            })
+        })
+    }
+ })
+
+const upload = multer({storage})
 
 
 route.get("/", (req, res) => {
@@ -67,12 +67,12 @@ app.use(morgan("common"))
 
 
 app.use("/.netlify/functions/index", route)
-// app.use("/api/users", userRoute)
-// app.use("/api/auth", authRoute)
-// app.use("/api/posts", postRoute)
-// app.use("/api/conversations", conversationRoute)
-// app.use("/api/messages", messageRoute)
-// app.use("/api/upload", imageRoute(upload))
+app.use("/.netlify/functions/index/api/users", userRoute)
+app.use("/.netlify/functions/index/api/auth", authRoute)
+app.use("/.netlify/functions/index/api/posts", postRoute)
+app.use("/.netlify/functions/index/api/conversations", conversationRoute)
+app.use("/.netlify/functions/index/api/messages", messageRoute)
+app.use("/.netlify/functions/index/api/upload", imageRoute(upload))
 
 // app.use("/api/users", userRoute)
 // app.use("/api/auth", authRoute)
